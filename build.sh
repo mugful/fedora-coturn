71932add852824d6a77a5817bc8c9a118711536f
Jiří Stránský <jistr@jistr.com>
2018-07-13 19:05:50 +0200

    Switch to copr repo with builds for F28

--- build.sh
+++ build.sh
@@ -3,16 +3,16 @@
 set -euxo pipefail
 
 FEDORA_RELEASE=$(cat /etc/fedora-release | awk '{ print $3; }')
-curl -o /etc/yum.repos.d/lorbus-coturn-fedora-$FEDORA_RELEASE.repo https://copr.fedorainfracloud.org/coprs/lorbus/coturn/repo/fedora-$FEDORA_RELEASE/lorbus-coturn-fedora-$FEDORA_RELEASE.repo
-yum clean all
+curl -o /etc/yum.repos.d/logic-coturn-fedora-$FEDORA_RELEASE.repo https://copr.fedorainfracloud.org/coprs/logic/coturn/repo/fedora-$FEDORA_RELEASE/logic-coturn-fedora-$FEDORA_RELEASE.repo
+dnf clean all
 
 useradd -u 43478 coturn
 
-yum -y install coturn
+dnf -y install turnserver
 
-chown -R coturn: /etc/coturn
+chown -R coturn: /etc/turnserver
 # Lockdown by default, prevent from running a world-open TURN server
 # by accident. This should force the operator to mount in their own
 # config file into the right place.
-cp /etc/coturn/turnserver.conf /etc/coturn/turnserver.conf.example
-echo 'listening-ip=127.0.0.1' > /etc/coturn/turnserver.conf
+cp /etc/turnserver/turnserver.conf /etc/turnserver/turnserver.conf.example
+echo 'listening-ip=127.0.0.1' > /etc/turnserver/turnserver.conf
